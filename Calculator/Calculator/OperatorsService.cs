8c6366f130bdaf2dfaf5abd53237ace89ac6ac00
AlehandroEr <ershova_mmikn114@mail.ru>
2018-11-24 01:05:01 +0500

    Really add OperatorService class
    In the previous commit that class is empty.

--- Calculator/Calculator/OperatorsService.cs
+++ Calculator/Calculator/OperatorsService.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Calculator
 {
-    class OperatorsService
+    public class OperatorsService : IOperatorsService
     {
+        public Dictionary<char, Func<double, double, double>> Operations { get; }
+
+        public OperatorsService()
+        {
+            Operations = new Dictionary<char, Func<double, double, double>>
+            {
+                {'+', (y, x) => x + y},
+                {'-', (y, x) => x - y},
+                {'*', (y, x) => x * y},
+                {'/', (y, x) => x / y}
+            };
+        }
+
+        public int GetPriority(char c)
+        {
+            switch (c)
+            {
+                case '+': return 0;
+                case '-': return 0;
+                case '*': return 1;
+                case '/': return 1;
+                default: throw new ArgumentException();
+            }
+        }
+
+        public bool IsOperator(char c)
+        {
+            return "+-/*()".IndexOf(c) != -1;
+        }
+
+        public bool IsSeparator(char c)
+        {
+            return " =".IndexOf(c) != -1;
+        }
     }
 }
