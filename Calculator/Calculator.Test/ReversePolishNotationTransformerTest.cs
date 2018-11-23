26f29364b612e74b4a73647dff5f2607201758bc
AlehandroEr <ershova_mmikn114@mail.ru>
2018-11-24 00:41:17 +0500

    Add test for ReversePolishNatationTransformer class

---
+++ Calculator/Calculator.Test/ReversePolishNotationTransformerTest.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace Calculator.Test
+{
+    public class ReversePolishNotationTransformerTest
+    {
+        [Fact]
+        public void TransformExpression()
+        {
+            var expression = "2 + 3";
+
+            var transformer = new ReversePolishNotationTransformer();
+
+            var transformedExpression = transformer.TransformExpression(expression);
+
+            Assert.Equal("2 3 + ", transformedExpression);
+        }
+    }
+}
