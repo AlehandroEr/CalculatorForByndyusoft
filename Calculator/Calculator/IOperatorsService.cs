e64bd48c27a8330dae24bb29b0b75e3456a55fcf
AlehandroEr <ershova_mmikn114@mail.ru>
2018-11-24 00:53:44 +0500

    Add OperatorsService class
    It works with arithmetic operators, separators and brackets.

---
+++ Calculator/Calculator/IOperatorsService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Calculator
+{
+    public interface IOperatorsService
+    {
+        Dictionary<char, Func<double, double, double>> Operations { get; }
+
+        int GetPriority(char c);
+        bool IsOperator(char c);
+        bool IsSeparator(char c);
+    }
+}
