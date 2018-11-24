29ea999c983ae24f2f1e6905ef8544e66d1728fb
AlehandroEr <ershova_mmikn114@mail.ru>
2018-11-24 19:33:34 +0500

    Update ReversePolishNotationTransformerTest
    Change entered expression. Use all of allowed operators.

--- Calculator/Calculator.Test/ReversePolishNotationTransformerTest.cs
+++ Calculator/Calculator.Test/ReversePolishNotationTransformerTest.cs
@@ -8,34 +8,78 @@ namespace Calculator.Test
         [Fact]
         public void ReturnReversePolishNotationExpression()
         {
-            var expression = "2 + 3";
+            var expression = "(1 + 10*2.5) / 10 - 1 = ";
 
             var operatorsService = new Mock<IOperatorsService>();
 
             operatorsService.Setup(m => m.GetPriority('+'))
                 .Returns(0);
+            operatorsService.Setup(m => m.GetPriority('-'))
+                .Returns(0);
+            operatorsService.Setup(m => m.GetPriority('*'))
+                .Returns(1);
+            operatorsService.Setup(m => m.GetPriority('/'))
+                .Returns(1);
+
+            operatorsService.Setup(m => m.IsSeparator('('))
+                .Returns(false);
+            operatorsService.Setup(m => m.IsSeparator(')'))
+                .Returns(false);
             operatorsService.Setup(m => m.IsSeparator(' '))
                 .Returns(true);
+            operatorsService.Setup(m => m.IsSeparator('='))
+                .Returns(true);
+            operatorsService.Setup(m => m.IsSeparator('.'))
+                .Returns(false);
+            operatorsService.Setup(m => m.IsSeparator('0'))
+                .Returns(false);
+            operatorsService.Setup(m => m.IsSeparator('1'))
+                .Returns(false);
             operatorsService.Setup(m => m.IsSeparator('2'))
                 .Returns(false);
-            operatorsService.Setup(m => m.IsSeparator('3'))
+            operatorsService.Setup(m => m.IsSeparator('5'))
                 .Returns(false);
             operatorsService.Setup(m => m.IsSeparator('+'))
                 .Returns(false);
-            operatorsService.Setup(m => m.IsOperator('2'))
+            operatorsService.Setup(m => m.IsSeparator('-'))
                 .Returns(false);
-            operatorsService.Setup(m => m.IsOperator('3'))
+            operatorsService.Setup(m => m.IsSeparator('*'))
                 .Returns(false);
+            operatorsService.Setup(m => m.IsSeparator('/'))
+                .Returns(false);
+
+            operatorsService.Setup(m => m.IsOperator('('))
+                .Returns(true);
+            operatorsService.Setup(m => m.IsOperator(')'))
+                .Returns(true);
             operatorsService.Setup(m => m.IsOperator(' '))
                 .Returns(false);
+            operatorsService.Setup(m => m.IsOperator('='))
+                .Returns(false);
+            operatorsService.Setup(m => m.IsOperator('.'))
+                .Returns(false);
+            operatorsService.Setup(m => m.IsOperator('0'))
+                .Returns(false);
+            operatorsService.Setup(m => m.IsOperator('1'))
+                .Returns(false);
+            operatorsService.Setup(m => m.IsOperator('2'))
+                .Returns(false);
+            operatorsService.Setup(m => m.IsOperator('5'))
+                .Returns(false);
             operatorsService.Setup(m => m.IsOperator('+'))
+                .Returns(true);
+            operatorsService.Setup(m => m.IsOperator('-'))
+                .Returns(true);
+            operatorsService.Setup(m => m.IsOperator('*'))
+                .Returns(true);
+            operatorsService.Setup(m => m.IsOperator('/'))
                 .Returns(true);
 
             var transformer = new ReversePolishNotationTransformer(operatorsService.Object);
 
             var transformedExpression = transformer.TransformExpression(expression);
 
-            Assert.Equal("2 3 + ", transformedExpression);
+            Assert.Equal("1 10 2.5 * + 10 / 1 - ", transformedExpression);
         }
     }
 }
