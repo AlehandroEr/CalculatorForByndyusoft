7cf60041720bafcad775464e2bf8acd51534e43b
AlehandroEr <ershova_mmikn114@mail.ru>
2018-11-24 18:48:34 +0500

    Add NinjectRegistrations class
    The class is NinjectModule that defines all dependencies for IoC-container.

---
+++ Calculator/Calculator/NinjectRegistrations.cs
@@ -0,0 +1,15 @@
+﻿using Ninject.Modules;
+
+namespace Calculator
+{
+    public class Bindings : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<Calculator>().ToSelf();
+            Bind<IOperatorsService>().To<OperatorsService>();
+            Bind<IExpressionTransformer>().To<ReversePolishNotationTransformer>();
+            Bind<IExpressionCalculator>().To<ReversePolishNotationCalculator>();
+        }
+    }
+}
