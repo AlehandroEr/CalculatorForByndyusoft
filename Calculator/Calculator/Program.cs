6b352255bafe48661df1c82787bb8377caab9a0c
AlehandroEr <ershova_mmikn114@mail.ru>
2018-11-24 18:59:51 +0500

    Add code to method Main
    Create IoC-container, root object and calculate expression.

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace Calculator
+﻿using System;
+using System.Reflection;
+using Ninject;
+
+namespace Calculator
 {
     class Program
     {
         static void Main(string[] args)
         {
+            IKernel kernel = new StandardKernel();
+            kernel.Load(Assembly.GetExecutingAssembly());
+
+            var calculator = kernel.Get<Calculator>();
+
+            Console.Write("Введите выражение: ");
+
+            var result = calculator.CalculateResultOfExpression(Console.ReadLine());
+
+            Console.WriteLine("Результат: " + result);
+            Console.ReadKey();
         }
     }
 }
